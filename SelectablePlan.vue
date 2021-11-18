b7ca42aec4f792c83df6d973a3a9bd36e3e061cc
muellejo <57995483+muellejo@users.noreply.github.com>
2021-11-18 09:27:54 +0100

    Add files via upload

---
+++ SelectablePlan.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+      <div v-if=" $t('products.product'+ this.id + '.productName' + this.id) != 'products.product'+ this.id + '.productName' + this.id">
+          <div class="mx-auto py-8 md:flex md:justify-between md:flex-wrap">
+              <div class="w-48 bg-white rounded-lg shadow-md px-6 py-4 cursor-pointer mb-8 hover:bg-blue-100 focus:outline-none focus:shadow-outline-green">
+                <div id="product">
+                  
+                  <section id="Name">
+                    <div class="flex justify-between items-center mb-3">
+                      <h1 class="uppercase text-base tracking-wide text-gray-700 font-bold my-2"> {{ $t('products.product'+ this.id + '.productName' + this.id)}}
+                      </h1>
+                    </div>
+                  </section>
+                  
+                  <section id="Description">
+                    <div class="mb-1 text-gray-800">
+                      <span class="text-base tracking-wide text-gray-700 my-2">{{ $t('products.product'+ this.id + '.productDesc' + this.id)}}</span>
+                    </div>
+                  </section>
+
+                  <section id="Size">
+                    <div v-if="$t('products.product'+ this.id + '.productType' + this.id) === 'Bike'">
+                      <span class="text-base tracking-wide text-gray-700 my-2">
+                        {{ $t('products.product'+ this.id + '.productSize' + this.id) + " " + $t('productSizeBike')}}
+                      </span>
+                    </div>
+                    <div v-else-if="$t('products.product'+ this.id + '.productType' + this.id) === 'Part'">
+                      <span class="text-base tracking-wide text-gray-700 my-2">
+                        {{ $t('products.product'+ this.id + '.productSize' + this.id) + " " + $t('productSizePart')}}
+                      </span>
+                    </div>
+                    <div v-else>
+                      <span class="text-base tracking-wide text-gray-700 my-2">
+                        {{ $t('productSizeOneSize')}}
+                      </span>
+                    </div>
+                  </section>
+
+                  <section id="Price">
+                      <div v-if="$t('currency') === '€'">
+                        <span class="text-l font-bold text-gray-700">{{  parseInt($t('products.product'+ this.id + '.productPrice' + this.id)) * 0.84 }}</span>
+                        <span class="text-xl text-gray-700">{{ " " + $t('currency') }}</span>
+                      </div>
+                      <div v-else>
+                        <span class="text-l font-bold text-gray-700">{{  parseInt($t('products.product'+ this.id + '.productPrice' + this.id))}}</span>
+                        <span class="text-xl text-gray-700">{{ " " + $t('currency') }}</span>
+                      </div>
+                  </section>
+                </div>
+              </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  props: ['id'],
+}
+</script>
+
---
+++ components/LanguageSwitcher.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="relative">
+    <button
+      href="#"
+      class="flex items-center"
+      @click="toggleVisibility"
+      @keydown.space.exact.prevent="toggleVisibility"
+      @keydown.esc.exact="hideDropdown"
+      @keydown.shift.tab="hideDropdown"
+      @keydown.up.exact.prevent="startArrowKeys"
+      @keydown.down.exact.prevent="startArrowKeys"
+    >
+      <img :src="`/flag_${$i18n.locale}.png`" alt="flag" class="w-8 h-8">
+      <span class="ml-2">{{ $i18n.locale.toUpperCase() }}</span>
+      <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"></path></svg>
+    </button>
+    <transition name="dropdown-fade">
+      <ul v-on-clickaway="hideDropdown" v-if="isVisible" ref="dropdown" class="absolute normal-case z-30 font-normal xs:left-0 lg:right-0 bg-white shadow overflow-hidden rounded w-48 border mt-2 py-1 lg:z-20">
+        <li>
+          <a
+            href="#"
+            @click.prevent="setLocale('en')"
+            ref="account"
+            class="flex items-center px-3 py-3 hover:bg-gray-200"
+            @keydown.up.exact.prevent=""
+            @keydown.tab.exact="focusNext(false)"
+            @keydown.down.exact.prevent="focusNext(true)"
+            @keydown.esc.exact="hideDropdown"
+          >
+            <img src="/flag_en.png" alt="english flag" class="h-8 w-8">
+            <span class="ml-2">{{$t("languageSwitcher.en")}}</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            @click.prevent="setLocale('de')"
+            class="flex items-center px-3 py-3 hover:bg-gray-200"
+            @keydown.shift.tab="focusPrevious(false)"
+            @keydown.up.exact.prevent="focusPrevious(true)"
+            @keydown.down.exact.prevent=""
+            @keydown.tab.exact="hideDropdown"
+            @keydown.esc.exact="hideDropdown"
+          >
+            <img src="/flag_de.png" alt="german flag" class="h-8 w-8">
+            <span class="ml-2">{{$t("languageSwitcher.de")}}</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            @click.prevent="setLocale('fr')"
+            class="flex items-center px-3 py-3 hover:bg-gray-200"
+            @keydown.shift.tab="focusPrevious(false)"
+            @keydown.up.exact.prevent="focusPrevious(true)"
+            @keydown.down.exact.prevent=""
+            @keydown.tab.exact="hideDropdown"
+            @keydown.esc.exact="hideDropdown"
+          >
+            <img src="/flag_fr.png" alt="french flag" class="h-8 w-8">
+            <span class="ml-2">{{$t("languageSwitcher.fr")}}</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            @click.prevent="setLocale('eu')"
+            class="flex items-center px-3 py-3 hover:bg-gray-200"
+            @keydown.shift.tab="focusPrevious(false)"
+            @keydown.up.exact.prevent="focusPrevious(true)"
+            @keydown.down.exact.prevent=""
+            @keydown.tab.exact="hideDropdown"
+            @keydown.esc.exact="hideDropdown"
+          >
+            <img src="/flag_eu.png" alt="eu flag" class="h-8 w-8">
+            <span class="ml-2">{{$t("languageSwitcher.eu")}}</span>
+          </a>
+        </li>
+      </ul>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { mixin as clickaway } from 'vue-clickaway'
+
+export default {
+  mixins: [ clickaway ],
+  data() {
+    return {
+      isVisible: false,
+      focusedIndex: 0,
+    }
+  },
+  methods: {
+    toggleVisibility() {
+      this.isVisible = !this.isVisible
+    },
+    hideDropdown() {
+      this.isVisible = false
+      this.focusedIndex = 0
+    },
+    startArrowKeys() {
+      if (this.isVisible) {
+        // this.$refs.account.focus()
+        this.$refs.dropdown.children[0].children[0].focus()
+      }
+    },
+    focusPrevious(isArrowKey) {
+      this.focusedIndex = this.focusedIndex - 1
+      if (isArrowKey) {
+        this.focusItem()
+      }
+    },
+    focusNext(isArrowKey) {
+      this.focusedIndex = this.focusedIndex + 1
+      if (isArrowKey) {
+        this.focusItem()
+      }
+    },
+    focusItem() {
+      this.$refs.dropdown.children[this.focusedIndex].children[0].focus()
+    },
+    setLocale(locale) {
+      this.$i18n.locale = locale
+      this.$router.push({
+        params: { lang: locale }
+      })
+      this.hideDropdown()
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+  .dropdown-fade-enter-active, .dropdown-fade-leave-active {
+    transition: all .1s ease-in-out;
+  }
+  .dropdown-fade-enter, .dropdown-fade-leave-to {
+    opacity: 0;
+    transform: translateY(-12px);
+  }
+</style>
+
